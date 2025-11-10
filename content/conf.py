79308671020e8d3228517604815efe566359a09a
Ashwin V. Mohanan <ashwin.mohanan@enccs.se>
2025-11-10 20:57:11 +0100

    Reduce logo size

--- content/conf.py
+++ content/conf.py
@@ -90,7 +90,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-# html_css_files = ["overrides.css"]
+html_css_files = ["overrides.css"]
 html_favicon = str((HERE / "_static" / "favicon.ico").resolve())
 html_theme_options = {
     "light_logo": "ENCCS_logo_light.png",
